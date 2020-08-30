03e631b6e0f899488259a8629ecf4541e254f5b2
mbfowler <matthew.fowler@procore.com>
2020-08-29 23:16:27 -0700

    working server, fixed lock

--- src/server.js
+++ src/server.js
@@ -4,40 +4,14 @@ const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser')
 const app = express();
-const randomChar = require('./randomCharacter');
 
 const router = express.Router();
 router.get('/', (req, res) => {
     res.json({
-        "text": randomChar()
+        "text": "hello"
     });
 });
-router.post('/', (req, res) => {
-    const character = randomChar()
-    console.log({ body: req.body })
-    res.json({
-        "response_type": "in_channel",
-        "text": character,
-        "blocks": [
-            {
-                "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": `:crystal_ball: <@${req.body.user_id}> summoned me!`
-                    }
-                ]
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": character,
-                }
-            }
-        ]
-    })
-});
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
