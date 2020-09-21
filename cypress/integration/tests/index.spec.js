e6bea1efa70f2c6996733a62e5eb24c2833936eb
mbfowler <mbfowler@gmail.com>
2020-09-21 15:44:43 -0700

    add first index and api cypress tests

---
+++ cypress/integration/tests/index.spec.js
@@ -0,0 +1,17 @@
+context('On index page', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('clicking the get new melody button refreshes the page with a new query param', () => {
+    cy.url().then(($url) => {
+      const origURL = $url
+      cy.get('button').contains('Get me a new melody!').click()
+      cy.url().should('not.equal', origURL)
+    })
+  })
+
+  // it('github footer button navigates to melody-maker project', () => {
+  //   cy.get('[alt="GitHub Icon"]').should('have.attr', 'href', 'https://github.com/mbfowler/melody-maker')
+  // })
+})
