9fb98840c7a0bcc942f229027122b728167bee6f
Matthew Fowler <mbfowler@gmail.com>
2020-10-03 10:27:45 -0700

    cypress production workflow (#14)
    * cypress workflow
    
    Breaking the cypress ci flow into its own file
    
    * add cypress env configs for prod and dev
    
    * delete global cypress config params
    
    * update cypress workflow explicit config file
    
    * try with command to configFile
    
    * add npx command
    
    * add newlines

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,10 +12,23 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile (file) {
+  const pathToConfigFile = path.resolve('.', 'cypress/config', `${file}.json`)
+  console.log(pathToConfigFile)
+  return fs.readJson(pathToConfigFile)
+}
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  // accept a configFile value or use development by default
+  const file = config.env.configFile || 'development'
+
+  return getConfigurationByFile(file)
 }
