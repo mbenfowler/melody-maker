9282d159af8afe6f17614d23bf8fdea6dd6c8746
mbfowler <mbfowler@gmail.com>
2020-10-15 15:10:47 -0700

    remove netlify-plugin-cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,11 +15,11 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-function getConfigurationByFile (file) {
-  const pathToConfigFile = path.resolve('.', 'cypress/config', `${file}.json`)
-  console.log(pathToConfigFile)
-  return fs.readJson(pathToConfigFile)
-}
+// function getConfigurationByFile (file) {
+//   const pathToConfigFile = path.resolve('.', 'cypress/config', `${file}.json`)
+//   console.log(pathToConfigFile)
+//   return fs.readJson(pathToConfigFile)
+// }
 
 /**
  * @type {Cypress.PluginConfig}
